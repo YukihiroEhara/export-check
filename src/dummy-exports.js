a7260f3f0bf7dc730c705e85b5724440720b77c6
zare926 <ilove.boc926@gmail.com>
2022-07-20 18:03:40 +0900

    test

--- src/dummy-exports.js
+++ src/dummy-exports.js
@@ -5,49 +5,70 @@ const arrayChange = (str) => {
 };
 
 const awsmobile = {
-  aws_project_region: process.env.REACT_APP_AWS_REGION,
+  aws_project_region:
+    process.env.REACT_APP_AWS_REGION === undefined
+      ? "ap-northeast-1"
+      : process.env.REACT_APP_AWS_REGION,
   aws_appsync_graphqlEndpoint:
-    process.env.REACT_APP_AWS_APPSYNC_GRAPHQLENDPOINT,
-  aws_appsync_region: process.env.REACT_APP_AWS_REGION,
+    process.env.REACT_APP_AWS_APPSYNC_GRAPHQLENDPOINT === undefined
+      ? "https://ifmwdqke7jbhnieekdezk5vz7m.appsync-api.ap-northeast-1.amazonaws.com/graphql"
+      : process.env.REACT_APP_AWS_APPSYNC_GRAPHQLENDPOINT,
+  aws_appsync_region:
+    process.env.REACT_APP_AWS_REGION === undefined
+      ? "ap-northeast-1"
+      : process.env.REACT_APP_AWS_REGION,
   aws_appsync_authenticationType:
-    process.env.REACT_APP_AWS_APPSYNC_AUTHENTICATIONTYPE,
-  //   aws_appsync_apiKey: process.env.REACT_APP_AWS_APPSYNC_APIKEY,
+    process.env.REACT_APP_AWS_APPSYNC_AUTHENTICATIONTYPE === undefined
+      ? "AMAZON_COGNITO_USER_POOLS"
+      : process.env.REACT_APP_AWS_APPSYNC_AUTHENTICATIONTYPE,
   aws_cognito_identity_pool_id:
-    process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
-  aws_cognito_region: process.env.REACT_APP_AWS_REGION,
-  aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
-  // aws_user_pools_id: "ap-northeast-1_JxPVesXaZ",
+    process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID === undefined
+      ? "ap-northeast-1:963809c2-89e9-496f-87e6-17a160bd5a69"
+      : process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
+  aws_cognito_region:
+    process.env.REACT_APP_AWS_REGION === undefined
+      ? "ap-northeast-1"
+      : process.env.REACT_APP_AWS_REGION,
+  aws_user_pools_id:
+    process.env.REACT_APP_AWS_USER_POOLS_ID === undefined
+      ? "ap-northeast-1_JxPVesXaZ"
+      : process.env.REACT_APP_AWS_USER_POOLS_ID,
   aws_user_pools_web_client_id:
-    process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
-  // aws_user_pools_web_client_id: "6c9pfcg7d5oubnq28daakn68f1",
-  //   oauth: process.env.REACT_APP_OAUTH,
+    process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID === undefined
+      ? "6c9pfcg7d5oubnq28daakn68f1"
+      : process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
   oauth: {},
-  //   aws_cognito_username_attributes: [],
-  aws_cognito_username_attributes: arrayChange(
-    process.env.REACT_APP_AWS_COGNITO_USERNAME_ATTRIBUTES
-  ),
-  //   aws_cognito_social_providers: [],
-  aws_cognito_social_providers: arrayChange(
-    process.env.REACT_APP_AWS_COGNITO_SOCIAL_PROVIDERS
-  ),
-  aws_cognito_signup_attributes: arrayChange(
-    process.env.REACT_APP_AWS_COGNITO_SIGNUP_ATTRIBUTES
-  ),
+  aws_cognito_username_attributes:
+    process.env.REACT_APP_AWS_COGNITO_USERNAME_ATTRIBUTES === undefined
+      ? ["EMAIL"]
+      : arrayChange(process.env.REACT_APP_AWS_COGNITO_USERNAME_ATTRIBUTES),
+  aws_cognito_social_providers:
+    process.env.REACT_APP_AWS_COGNITO_SOCIAL_PROVIDERS === undefined
+      ? []
+      : arrayChange(process.env.REACT_APP_AWS_COGNITO_SOCIAL_PROVIDERS),
+  aws_cognito_signup_attributes:
+    process.env.REACT_APP_AWS_COGNITO_SIGNUP_ATTRIBUTES === undefined
+      ? ["EMAIL"]
+      : arrayChange(process.env.REACT_APP_AWS_COGNITO_SIGNUP_ATTRIBUTES),
   aws_cognito_mfa_configuration:
-    process.env.REACT_APP_AWS_COGNITO_MFA_CONFIGURATION,
-  aws_cognito_mfa_types: arrayChange(
-    process.env.REACT_APP_AWS_COGNITO_MFA_TYPES
-  ),
+    process.env.REACT_APP_AWS_COGNITO_MFA_CONFIGURATION === undefined
+      ? "OFF"
+      : process.env.REACT_APP_AWS_COGNITO_MFA_CONFIGURATION,
+  aws_cognito_mfa_types:
+    process.env.REACT_APP_AWS_COGNITO_MFA_TYPES === undefined
+      ? ["SMS"]
+      : arrayChange(process.env.REACT_APP_AWS_COGNITO_MFA_TYPES),
   aws_cognito_password_protection_settings: {
     passwordPolicyMinLength: 8,
-    // passwordPolicyMinLength: process.env.REACT_APP_PASSWORDPOLICYMINLENGTH,
-    passwordPolicyCharacters: arrayChange(
-      process.env.REACT_APP_PASSWORDPOLICYCHARACTERS
-    ),
+    passwordPolicyCharacters:
+      process.env.REACT_APP_PASSWORDPOLICYCHARACTERS === undefined
+        ? []
+        : arrayChange(process.env.REACT_APP_PASSWORDPOLICYCHARACTERS),
   },
-  aws_cognito_verification_mechanisms: arrayChange(
-    process.env.REACT_APP_AWS_COGNITO_VERIFICATION_MECHANISMS
-  ),
+  aws_cognito_verification_mechanisms:
+    process.env.REACT_APP_AWS_COGNITO_VERIFICATION_MECHANISMS === undefined
+      ? ["EMAIL"]
+      : arrayChange(process.env.REACT_APP_AWS_COGNITO_VERIFICATION_MECHANISMS),
 };
 
 export default awsmobile;
